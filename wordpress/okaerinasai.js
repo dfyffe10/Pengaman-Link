8ea6aa2cf8c0646cf28ed26dbe367096e056e211
Darryl Fyffe <53137481+dfyffe10@users.noreply.github.com>
2019-10-31 11:19:34 +0700

    Update okaerinasai.js

--- wordpress/okaerinasai.js
+++ wordpress/okaerinasai.js
@@ -20,7 +20,7 @@ j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);var x=Math.floor((Math.random()*1)+ 1);
 if(x=="1"){xxx="peritoneal-mesothelioma/"}
-a_to_ve[i].href="http://indonesia-security.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+a_to_ve[i].href="http://indonesia-security.com/"+xxx+"?secure="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
